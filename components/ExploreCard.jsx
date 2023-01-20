786be16675d4aaacbb2bb0a6776aa25e49417d00
DESKTOP-L5IKPBH\presentation <vishal.singhaniya@codenia.in>
2023-01-20 13:21:12 +0530

    Update ExploreCard.jsx

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -33,9 +33,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
             className="w-1/2 h-1/2 object-contain"
           />
         </div>
-        <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
+        {/* <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
           Operating Model  
-        </p>
+        </p> */}
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
         </h2>
